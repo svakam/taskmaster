ca20194958b699107107c576f2841a19827585b4
Vik Akam <vikramakam@hotmail.com>
2020-02-11 13:04:29 -0800

    progress on add page

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,14 +2,57 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    static String TAG = "va.main";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // setContentView, R
+        // actually set contents to be the activity_main XML file
         setContentView(R.layout.activity_main);
+        // setup work
+        // logging: verbose, debug, info, warning, error, wtf
+        Log.w(TAG, "we are in onCreate");
+        final Button addTask = findViewById(R.id.addTask);
+        // anonymous inner class: define a class that implements View.OnClickListener, right here inline
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddTask.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "stopped");
     }
 }
-
